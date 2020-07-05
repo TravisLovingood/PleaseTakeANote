3126a979a8c1c5e66149b21b1615a7ecec7d2c11
TravisLovingood <61667953+TravisLovingood@users.noreply.github.com>
2020-07-05 16:01:30 -0400

    fixed routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -6,7 +6,7 @@ router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname,"../public/notes.html"));
 });
 
-//for indexedDB.
+//for index
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname,"../public/index.html"));
 });
